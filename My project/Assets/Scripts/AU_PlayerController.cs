c46e0e55605d7d27a2f440739ce08a65d9780d83
RobbeDH <59093989+RobbeDH@users.noreply.github.com>
2023-02-14 13:52:57 +0100

    imposter ඞඞඞඞඞ

--- My project/Assets/Scripts/AU_PlayerController.cs
+++ My project/Assets/Scripts/AU_PlayerController.cs
@@ -3,59 +3,147 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-//Controls the player movement and animation
-
 public class AU_PlayerController : MonoBehaviour
 {
+    [SerializeField] bool hasControl;
+    public static AU_PlayerController localPlayer;
+    
+
     //Components
     Rigidbody myRB;
-    Transform myAvatar;
     Animator myAnim;
+    Transform myAvatar;
     //Player movement
     [SerializeField] InputAction WASD;
     Vector2 movementInput;
     [SerializeField] float movementSpeed;
+    //Player Color
+    static Color myColor;
+    SpriteRenderer myAvatarSprite;
 
+    //Role
+    [SerializeField] bool isImposter; // SUS AF ඞඞඞඞඞඞඞඞ
+    [SerializeField] InputAction KILL;
+    
+
+    AU_PlayerController target;
+    [SerializeField] Collider myCollider;
+
+    bool isDead;
+
+
+    private void Awake()
+    {
+        KILL.performed += KillTarget;
+    }
 
     private void OnEnable()
     {
         WASD.Enable();
-
+        KILL.Enable();
     }
 
     private void OnDisable()
     {
         WASD.Disable();
-
+        KILL.Disable();
     }
 
 
     // Start is called before the first frame update
     void Start()
     {
+        if(hasControl)
+        {
+            localPlayer = this;
+        }
+
         myRB = GetComponent<Rigidbody>();
-
-        myAvatar = transform.GetChild(0);
-
         myAnim = GetComponent<Animator>();
+        myAvatar = transform.GetChild(0);
+        myAvatarSprite = myAvatar.GetComponent<SpriteRenderer>();
+        if (myColor == Color.clear)
+            myColor = Color.white;
+        if (!hasControl)
+            return;
+        myAvatarSprite.color = myColor;
     }
 
     // Update is called once per frame
     void Update()
     {
-        movementInput = WASD.ReadValue<Vector2>();
+        if (!hasControl)
+            return;
 
+        movementInput = WASD.ReadValue<Vector2>();
+        myAnim.SetFloat("Speed", movementInput.magnitude);
         if (movementInput.x != 0)
         {
             myAvatar.localScale = new Vector2(Mathf.Sign(movementInput.x), 1);
         }
 
-        myAnim.SetFloat("Speed", movementInput.magnitude);
+        
+
     }
 
     private void FixedUpdate()
     {
         myRB.velocity = movementInput * movementSpeed;
+    }
+
+    public void SetColor(Color newColor)
+    {
+        myColor = newColor;
+        if (myAvatarSprite != null)
+        {
+            myAvatarSprite.color = myColor;
+        }
+    }
+
+    public void SetRole(bool newRole)
+    {
+        isImposter = newRole; // ඞ FUCKING SUSSY ඞ
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            AU_PlayerController tempTarget = other.GetComponent<AU_PlayerController>();
+            if (isImposter) // ඞ
+            {
+                if (tempTarget.isImposter) // ඞ
+                    return;
+                else
+                {
+                    target = tempTarget;
+                    //Debug.Log(target.name);
+                }
+            }
+        }
+    }
+
+    void KillTarget(InputAction.CallbackContext context)
+    {
+        if (context.phase == InputActionPhase.Performed) 
+        {
+            if (target == null)
+                return;
+            else
+            {
+                if (target.isDead)
+                    return;
+                transform.position = target.transform.position;
+                target.Die();
+                target = null;
+            }
+        }
+    }
+
+    public void Die()
+    {
+        myAnim.SetBool("IsDead", true);
+        myCollider.enabled = false;
     }
 
 }
