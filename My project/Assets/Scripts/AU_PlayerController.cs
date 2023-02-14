8eeabe93df453499fa3c873e81a8b3a47150dc89
RobbeDH <59093989+RobbeDH@users.noreply.github.com>
2023-02-14 11:04:52 +0100

    Fixed C

---
+++ My project/Assets/Scripts/AU_PlayerController.cs
@@ -0,0 +1,79 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AU_PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+//Controls the player movement and animation
+
+public class AU_PlayerController : MonoBehaviour
+{
+    //Components
+    Rigidbody myRB;
+    Transform myAvatar;
+    Animator myAnim;
+    //Player movement
+    [SerializeField] InputAction WASD;
+    Vector2 movementInput;
+    [SerializeField] float movementSpeed;
+
+
+    private void OnEnable()
+    {
+        WASD.Enable();
+
+    }
+
+    private void OnDisable()
+    {
+        WASD.Disable();
+
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRB = GetComponent<Rigidbody>();
+
+        myAvatar = transform.GetChild(0);
+
+        myAnim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movementInput = WASD.ReadValue<Vector2>();
+
+        if (movementInput.x != 0)
+        {
+            myAvatar.localScale = new Vector2(Mathf.Sign(movementInput.x), 1);
+        }
+
+        myAnim.SetFloat("Speed", movementInput.magnitude);
+    }
+
+    private void FixedUpdate()
+    {
+        myRB.velocity = movementInput * movementSpeed;
+    }
+
+}
