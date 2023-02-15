de4eb266d7dab6edb1636d3d577862ea7681a186
RobbeDH <59093989+RobbeDH@users.noreply.github.com>
2023-02-15 12:15:53 +0100

    schaduw fix

--- My project/Assets/Scripts/AU_PlayerController.cs
+++ My project/Assets/Scripts/AU_PlayerController.cs
@@ -183,7 +183,7 @@ public class AU_PlayerController : MonoBehaviour
         isDead = true;
 
         myAnim.SetBool("IsDead", isDead);
-        gameObject.layer = 9;
+        gameObject.layer = 6;
         myCollider.enabled = false;
     }
 
