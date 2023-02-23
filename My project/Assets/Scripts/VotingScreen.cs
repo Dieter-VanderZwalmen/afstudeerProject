ad0c4d9a9b5c073e98e439ff898cf7e531919c3a
MattiasWaem <75215270+MattiasWaem@users.noreply.github.com>
2023-02-23 13:46:50 +0100

    quick

--- My project/Assets/Scripts/VotingScreen.cs
+++ My project/Assets/Scripts/VotingScreen.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.UIElements;
+using UnityEngine.UI;
 
 
 public class VotingScreen : MonoBehaviour
