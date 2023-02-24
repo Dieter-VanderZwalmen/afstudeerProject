4e610c87139dda66ae6bc7afb6c5a1d70f62012d
RobbeDH <59093989+RobbeDH@users.noreply.github.com>
2023-02-24 11:05:16 +0100

    Fixed: eerste player is masterclient
    Eerste player dat op play klikt is de master client

--- My project/Assets/Scripts/QuickStartLobbyController.cs
+++ My project/Assets/Scripts/QuickStartLobbyController.cs
@@ -31,7 +31,9 @@ public class QuickStartLobbyController : MonoBehaviourPunCallbacks
         int randomRoomNumber = Random.Range(0, 10000);
         RoomOptions roomOps = new RoomOptions() { IsVisible = true, IsOpen = true, MaxPlayers = (byte)roomSize };
         PhotonNetwork.CreateRoom("Room" + randomRoomNumber, roomOps);
-        Debug.Log(randomRoomNumber);
+        //make master client
+        PhotonNetwork.SetMasterClient(PhotonNetwork.LocalPlayer);
+        Debug.Log(randomRoomNumber + " Je bent de master client");
     }
 
     public override void OnCreateRoomFailed(short returnCode, string message)
